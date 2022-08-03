0b1f6bac6a7e2d8ce90969daaa84c14f6e2f3716
root <root@localhost>
2022-08-03 15:02:20 -0600

    docker zfs

--- docker.nix
+++ docker.nix
@@ -1,11 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  system.activationScripts = {
+    docker-setup.text = ''
+      ${pkgs.zfs}/bin/zfs list rpool/varlib/docker >/dev/null 2>&1 || ${pkgs.zfs}/bin/zfs create rpool/varlib/docker
+    '';
+  };
+
   virtualisation = {
     docker = {
       enable = true;
       enableOnBoot = true;
       autoPrune.enable = true;
+      storageDriver = "zfs";
     };
   };
 }
