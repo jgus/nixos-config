f718aaa57f97818bd433a4befd80ff011ae6fb6b
root <root@localhost>
2022-08-08 20:24:17 -0600

    gen-interfaces

--- gen-interfaces.sh
+++ gen-interfaces.sh
@@ -2,7 +2,7 @@
 
 (
     echo "{ ... }: {"
-    for i in $(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}')
+    for i in $(ip link | awk -F: '$0 !~ "lo|vir|wl|wg|docker|^[^0-9]"{print $2;getline}')
     do
         echo "  networking.interfaces.${i}.useDHCP = true;"
     done
