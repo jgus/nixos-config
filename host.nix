e210aa197661737607d58a61937c89ed70654655
root <root@localhost>
2022-08-10 00:13:56 -0600

    persist host keys (for backup)

--- host.nix
+++ host.nix
@@ -3,6 +3,25 @@
 {
   time.timeZone = "America/Denver";
 
+  environment.etc = {
+    "ssh/ssh_host_ed25519_key" = {
+      source = ./.secrets/ssh/ssh_host_ed25519_key;
+      mode = "0600";
+    };
+    "ssh/ssh_host_ed25519_key.pub" = {
+      source = ./.secrets/ssh/ssh_host_ed25519_key.pub;
+      mode = "0644";
+    };
+    "ssh/ssh_host_rsa_key" = {
+      source = ./.secrets/ssh/ssh_host_rsa_key;
+      mode = "0600";
+    };
+    "ssh/ssh_host_rsa_key.pub" = {
+      source = ./.secrets/ssh/ssh_host_rsa_key.pub;
+      mode = "0644";
+    };
+  };
+
   networking = {
     hostName = "c240m3";
     hostId = "04b22318"; # head -c4 /dev/urandom | od -A none -t x4
