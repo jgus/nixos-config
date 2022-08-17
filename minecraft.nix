3b4566abb299ac5469c18cb7c1e4739ed037ce04
root <root@localhost>
2022-08-16 19:17:17 -0600

    minecraft wip

--- minecraft.nix
+++ minecraft.nix
@@ -3,6 +3,15 @@
 {
   imports = [ ./docker.nix ];
 
+  networking.firewall.allowedTCPPorts = [
+    25522
+    8123
+    19132
+    19133
+    25565
+    25565
+  ];
+
   users = {
     groups.minecraft = { gid = 996; };
     users.minecraft = {
@@ -12,48 +21,54 @@
     };
   };
 
-  environment.etc = lib.attrsets.mapAttrs' (name: value: lib.attrsets.nameValuePair ("minecraft/docker/${name}") { source = ./minecraft/docker/${name}; mode = "0444"; }) (builtins.readDir ./minecraft/docker);
+  environment.etc =
+    lib.attrsets.mapAttrs'
+      (name: value:
+        lib.attrsets.nameValuePair
+          ("minecraft/docker/${name}")
+          {
+            source = ./minecraft/docker/${name};
+            mode = "0444";
+          }
+      )
+      (builtins.readDir ./minecraft/docker);
 
-  system.activationScripts = {
-    minecraftSetup.text = ''
-      ${pkgs.docker}/bin/docker build \
-        --build-arg uid=$(id -u minecraft) \
-        --build-arg gid=$(id -g minecraft) \
-        --build-arg java_ver=17 \
-        -t minecraft \
-        /etc/minecraft/docker
+  systemd = {
+    services = {
+      minecraft = {
+        enable = true;
+        description = "Minecraft";
+        wantedBy = [ "multi-user.target" ];
+        requires = [ "network-online.target" ];
+        path = [ pkgs.docker pkgs.zfs ];
+        script = ''
+          zfs list rpool/varlib/minecraft >/dev/null 2>&1 || ( zfs create rpool/varlib/minecraft && chown minecraft:minecraft /var/lib/minecraft )
 
-      ${pkgs.zfs}/bin/zfs list rpool/varlib/minecraft >/dev/null 2>&1 || ( ${pkgs.zfs}/bin/zfs create rpool/varlib/minecraft && chown minecraft:minecraft /var/lib/minecraft )
-    '';
+          docker build \
+            --build-arg uid=$(id -u minecraft) \
+            --build-arg gid=$(id -g minecraft) \
+            --build-arg java_ver=17 \
+            -t minecraft \
+            /etc/minecraft/docker
+
+          docker run --rm --name minecraft \
+            -p 25522:22/tcp \
+            -p 8123:8123/tcp \
+            -p 19132:19132/udp \
+            -p 19133:19133/udp \
+            -p 25565:25565/udp \
+            -p 25565:25565/tcp \
+            -v /var/volumes/minecraft_config:/home/minecraft/config \
+            minecraft
+          '';
+        unitConfig = {
+          StartLimitIntervalSec = 0;
+        };
+        serviceConfig = {
+          Restart = "always";
+          RestartSec = 10;
+        };
+      };
+    };
   };
-
-  # systemd = {
-  #   services = {
-  #     landing = {
-  #       enable = true;
-  #       description = "Landing";
-  #       wantedBy = [ "multi-user.target" ];
-  #       requires = [ "network-online.target" ];
-  #       path = [ pkgs.docker pkgs.rsync ];
-  #       script = ''
-  #         rsync -arPL /etc/landing /tmp/
-  #         rsync -arPL /etc/nixos/.secrets/landing /tmp/
-  #         chmod a+x /tmp/landing/docker/entrypoint.sh
-  #         docker build -t ssh /tmp/landing/docker
-  #         /bin/sh -c "docker run --rm --name landing \
-  #           -p 22022:22/tcp \
-  #           -e AUTHORIZED_KEYS='$(cat /root/.ssh/id_rsa-backup.pub)' \
-  #           -v /tmp/landing/etc/ssh:/etc/ssh \
-  #           ssh"
-  #         '';
-  #       unitConfig = {
-  #         StartLimitIntervalSec = 0;
-  #       };
-  #       serviceConfig = {
-  #         Restart = "always";
-  #         RestartSec = 10;
-  #       };
-  #     };
-  #   };
-  # };
 }
--- minecraft/docker/entrypoint.sh
+++ minecraft/docker/entrypoint.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo -u minecraft -i /usr/local/bin/start.sh
+
+function cleanup()
+{
+    sudo -u minecraft -i /usr/local/bin/stop.sh
+}
+trap cleanup EXIT
+
 mkdir -p /var/run/sshd
-/usr/sbin/sshd -D -e
+/usr/sbin/sshd -D -d -e
