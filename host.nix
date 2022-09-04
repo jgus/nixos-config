577e76016f26b33295b56c28fd4abd914f67d507
root <root@localhost>
2022-09-03 19:02:39 -0600

    host

--- host.nix
+++ host.nix
@@ -4,7 +4,7 @@
   time.timeZone = "America/Denver";
 
   networking = {
-    hostName = "TODO";
-    hostId = "HOSTID"; # head -c4 /dev/urandom | od -A none -t x4
+    hostName = "s3k2";
+    hostId = "c8357f63"; # head -c4 /dev/urandom | od -A none -t x4
   };
 }
