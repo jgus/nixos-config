3ba45d0413b0a684c6ef8d3527467d83501c51e5
root <root@localhost>
2022-09-08 11:33:47 -0600

    syncthing

--- syncthing.nix
+++ syncthing.nix
@@ -17,21 +17,22 @@
         requires = [ "network-online.target" ];
         path = [ pkgs.docker ];
         script = ''
-          /bin/sh -c "docker run --rm --name syncthing \
-          -p 8384:8384 \
-          -p 22000:22000 \
-          -p 21027:21027/udp \
-          -e PUID=$(id -u josh) \
-          -e PGID=$(id -g josh) \
-          -e TZ=$(timedatectl show -p Timezone --value) \
-          -e UMASK_SET=002 \
-          -v /var/lib/syncthing:/config \
-          -v /home/josh/sync:/shares/Sync \
-          -v /d/photos:/shares/Photos \
-          -v /d/software/Tools:/shares/Tools \
-          -v /d/media/Comics:/shares/Comics \
-          -v /d/media/Music:/shares/Music \
-          lscr.io/linuxserver/syncthing"
+          docker pull lscr.io/linuxserver/syncthing
+          docker run --rm --name syncthing \
+            -p 8384:8384 \
+            -p 22000:22000 \
+            -p 21027:21027/udp \
+            -e PUID=$(id -u josh) \
+            -e PGID=$(id -g josh) \
+            -e TZ=$(timedatectl show -p Timezone --value) \
+            -e UMASK_SET=002 \
+            -v /var/lib/syncthing:/config \
+            -v /home/josh/sync:/shares/Sync \
+            -v /d/photos:/shares/Photos \
+            -v /d/software/Tools:/shares/Tools \
+            -v /d/media/Comics:/shares/Comics \
+            -v /d/media/Music:/shares/Music \
+            lscr.io/linuxserver/syncthing
           '';
         serviceConfig = {
           Restart = "on-failure";
