ca544780dd5fe5f0cac53ab1250ecf0501179ee0
root <root@localhost>
2022-09-08 11:33:47 -0600

    syncthing

--- syncthing.nix
+++ syncthing.nix
@@ -17,16 +17,17 @@
         requires = [ "network-online.target" ];
         path = [ pkgs.docker ];
         script = ''
-          /bin/sh -c "docker run --rm --name syncthing \
-          -p 8384:8384 \
-          -p 22000:22000 \
-          -p 21027:21027/udp \
-          -e PUID=$(id -u josh) \
-          -e PGID=$(id -g josh) \
-          -e TZ=$(timedatectl show -p Timezone --value) \
-          -e UMASK_SET=002 \
-          -v /var/lib/syncthing:/config \
-          lscr.io/linuxserver/syncthing"
+          docker pull lscr.io/linuxserver/syncthing
+          docker run --rm --name syncthing \
+            -p 8384:8384 \
+            -p 22000:22000 \
+            -p 21027:21027/udp \
+            -e PUID=$(id -u josh) \
+            -e PGID=$(id -g josh) \
+            -e TZ=$(timedatectl show -p Timezone --value) \
+            -e UMASK_SET=002 \
+            -v /var/lib/syncthing:/config \
+            lscr.io/linuxserver/syncthing
           '';
         serviceConfig = {
           Restart = "on-failure";
