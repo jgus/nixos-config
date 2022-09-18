30a7ac3190d6afc03992f358165d2cbf89552b50
root <root@localhost>
2022-09-18 15:08:30 -0600

    install

--- host.nix
+++ host.nix
@@ -5,6 +5,6 @@
 
   networking = {
     hostName = "TODO";
-    hostId = "HOSTID"; # head -c4 /dev/urandom | od -A none -t x4
+    hostId = "d115e877"; # head -c4 /dev/urandom | od -A none -t x4
   };
 }
