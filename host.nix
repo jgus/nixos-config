6fae2604796ddbe2d8a66f1537c6b0f526724fff
root <root@localhost>
2022-10-31 15:42:16 -0600

    default gateway

--- host.nix
+++ host.nix
@@ -6,5 +6,9 @@
   networking = {
     hostName = "sm1";
     hostId = "d115e877"; # head -c4 /dev/urandom | od -A none -t x4
+    defaultGateway = {
+      address = "172.22.0.1";
+      interface = "eno1";
+    };
   };
 }
