740f143bd828ab374863922af47a28f759d50b72
root <root@localhost>
2022-11-28 19:27:43 -0700

    interfaces

--- interfaces.nix
+++ interfaces.nix
@@ -4,13 +4,5 @@
   networking.interfaces.enp2s0f2.useDHCP = true;
   networking.interfaces.enp2s0f3.useDHCP = true;
   networking.interfaces.enp10s0f0.useDHCP = true;
-  networking.interfaces.enp10s0f1 = {
-    useDHCP = false;
-    ipv4.addresses = [
-      {
-        address = "172.22.1.9";
-        prefixLength = 16;
-      }
-    ];
-  };
+  networking.interfaces.enp10s0f1.useDHCP = true;
 }
