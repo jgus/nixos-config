f1b27f7aee220744c52fd128deee07e929d9116d
Josh Gustafson <j@gustafson.me>
2023-03-07 13:05:46 -0700

    perf

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -64,6 +64,6 @@
   # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
