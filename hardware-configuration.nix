c9c952be59f09896ae093e76ca0e1d5ed6f82e44
root <root@localhost>
2023-03-15 16:28:35 -0700

    gustafson-nas

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,40 +8,54 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "rpool";
-      fsType = "zfs"; options = [ "zfsutil" ];
+    { device = "r";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
     };
 
   fileSystems."/etc/nixos" =
-    { device = "rpool/nixos";
-      fsType = "zfs"; options = [ "zfsutil" ];
+    { device = "r/nixos";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
     };
 
   fileSystems."/home" =
-    { device = "rpool/home";
-      fsType = "zfs"; options = [ "zfsutil" ];
+    { device = "r/home";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
     };
 
   fileSystems."/root" =
-    { device = "rpool/home/root";
-      fsType = "zfs"; options = [ "zfsutil" ];
+    { device = "r/home/root";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+
+  fileSystems."/var/lib" =
+    { device = "r/varlib";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/89F3-431E";
+    { device = "/dev/disk/by-uuid/48C1-654B";
       fsType = "vfat";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/d15d2de6-897f-4b26-964c-5f6e3b843be8"; }
-      { device = "/dev/disk/by-uuid/e80642a0-b25e-4f19-ba04-cef25997bef2"; }
-    ];
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/e80642a0-b25e-4f19-ba04-cef25997bef2";
+      priority = 0;
+    }
+    { device = "/dev/disk/by-uuid/d15d2de6-897f-4b26-964c-5f6e3b843be8";
+      priority = 0;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -49,8 +63,9 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp0s21f0u5.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eth0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
 
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   # high-resolution display
   hardware.video.hidpi.enable = lib.mkDefault true;
--- interfaces.nix
+++ interfaces.nix
@@ -1,4 +1,4 @@
 { ... }: {
-  networking.interfaces.eth0.useDHCP = true;
   networking.interfaces.enp0s21f0u5.useDHCP = true;
+  networking.interfaces.enp2s0.useDHCP = true;
 }
