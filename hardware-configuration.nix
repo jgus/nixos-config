00434d3a37fc33942adacc5b39c2d64a60d72054
Josh Gustafson <j@gustafson.me>
2023-03-24 10:41:50 -0600

    cpu governer

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -70,6 +70,6 @@
   # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
