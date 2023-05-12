2807f190dbc83f357edcabf7f6030e19149df739
Josh Gustafson <j@gustafson.me>
2023-05-12 15:01:59 -0600

    guxbox-pi

--- host.nix
+++ host.nix
@@ -4,7 +4,7 @@
   time.timeZone = "America/Denver";
 
   networking = {
-    hostName = "TODO";
-    hostId = "HOSTID"; # head -c4 /dev/urandom | od -A none -t x4
+    hostName = "gusbox-pi";
+    hostId = "adfce7d8"; # head -c4 /dev/urandom | od -A none -t x4
   };
 }
