3b7b5d5b0ac71db000e0c72f196ea9ea9d2c51fe
root <root@localhost>
2023-06-25 21:21:15 -0600

    interfaces

--- interfaces.nix
+++ interfaces.nix
@@ -3,8 +3,4 @@
   networking.interfaces.eno2.useDHCP = true;
   networking.interfaces.eno3.useDHCP = true;
   networking.interfaces.eno4.useDHCP = true;
-  networking.interfaces.enp5s0f0.useDHCP = true;
-  networking.interfaces.enp5s0f1.useDHCP = true;
-  networking.interfaces.enp5s0f2.useDHCP = true;
-  networking.interfaces.enp5s0f3.useDHCP = true;
 }
