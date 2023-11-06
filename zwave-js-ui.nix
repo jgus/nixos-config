dc6449db623ddc5ba5753ed11364eeb5a34285e5
Josh Gustafson <j@gustafson.me>
2023-11-06 16:13:11 -0700

    zwave-js-ui

---
+++ zwave-js-ui.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, ... }:
+
+let
+  devices = {
+    pi-67dc75 = "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00";
+  };
+in
+{
+  imports = [ ./docker.nix ];
+
+  system.activationScripts = {
+    zwave-js-ui-setup.text = ''
+      [ -d /var/lib/zwave-js-ui ] || ( mkdir /var/lib/zwave-js-ui )
+    '';
+  };
+
+  networking.firewall = {
+    allowedTCPPorts = [ 8091 3000 ];
+  };
+
+  systemd = {
+    services = {
+      zwave-js-ui = {
+        enable = true;
+        description = "ZWave-JS-UI";
+        wantedBy = [ "multi-user.target" ];
+        requires = [ "network-online.target" ];
+        path = [ pkgs.docker ];
+        script = ''
+          docker container stop zwave-js-ui >/dev/null 2>&1 || true ; \
+          docker run --rm --name zwave-js-ui \
+            -p 8091:8091 \
+            -p 3000:3000 \
+            --device=${devices.${config.networking.hostName}}:/dev/zwave \
+            -e TZ=$(timedatectl show -p Timezone --value) \
+            -v /var/lib/zwave-js-ui:/usr/src/app/store \
+            zwavejs/zwave-js-ui
+          '';
+        serviceConfig = {
+          Restart = "on-failure";
+        };
+      };
+      zwave-js-ui-update = {
+        path = [ pkgs.docker ];
+        script = ''
+          if docker pull zwavejs/zwave-js-ui | grep "Status: Downloaded"
+          then
+            systemctl restart zwave-js-ui
+          fi
+        '';
+        serviceConfig = {
+          Type = "oneshot";
+        };
+        startAt = "hourly";
+      };
+    };
+  };
+}
