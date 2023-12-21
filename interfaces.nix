e4ae8392cbadea6fde8b16821e36ff2fdb0b24b2
root <root@localhost>
2023-12-21 15:16:21 -0700

    Add 10g card

--- interfaces.nix
+++ interfaces.nix
@@ -3,4 +3,6 @@
   networking.interfaces.eno2.useDHCP = true;
   networking.interfaces.eno3.useDHCP = true;
   networking.interfaces.eno4.useDHCP = true;
+  networking.interfaces.enp5s0f0.useDHCP = true;
+  networking.interfaces.enp5s0f1.useDHCP = true;
 }
