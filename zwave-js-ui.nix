4c732e80f9c52ca27bae4ff76859f2ef19170fcb
root <root@localhost>
2024-06-05 11:34:27 -0600

    docker migration - zwave

--- zwave-js-ui.nix
+++ zwave-js-ui.nix
@@ -7,6 +7,7 @@ let
     pi-67dbcd = "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00";
     pi-67dc75 = "/dev/serial/by-id/usb-Zooz_800_Z-Wave_Stick_533D004242-if00";
   };
+  image = "zwavejs/zwave-js-ui";
 in
 {
   imports = [ ./docker.nix ];
@@ -21,34 +22,32 @@ in
     allowedTCPPorts = [ 8091 3000 ];
   };
 
+  virtualisation.oci-containers.containers.zwave-js-ui = {
+    image = "${image}";
+    autoStart = true;
+    extraOptions = [
+      "--device=${devices.${config.networking.hostName}}:/dev/zwave"
+    ];
+    ports = [
+      "8091:8091"
+      "3000:3000"
+    ];
+    environment = {
+      TZ = "${config.time.timeZone}";
+    };
+    volumes = [
+      "/var/lib/zwave-js-ui:/usr/src/app/store"
+    ];
+  };
+
   systemd = {
     services = {
-      zwave-js-ui = {
-        enable = true;
-        description = "ZWave-JS-UI";
-        wantedBy = [ "multi-user.target" ];
-        requires = [ "network-online.target" ];
-        path = [ pkgs.docker ];
-        script = ''
-          docker container stop zwave-js-ui >/dev/null 2>&1 || true ; \
-          docker run --rm --name zwave-js-ui \
-            -p 8091:8091 \
-            -p 3000:3000 \
-            --device=${devices.${config.networking.hostName}}:/dev/zwave \
-            -e TZ=$(timedatectl show -p Timezone --value) \
-            -v /var/lib/zwave-js-ui:/usr/src/app/store \
-            zwavejs/zwave-js-ui
-          '';
-        serviceConfig = {
-          Restart = "on-failure";
-        };
-      };
       zwave-js-ui-update = {
         path = [ pkgs.docker ];
         script = ''
-          if docker pull zwavejs/zwave-js-ui | grep "Status: Downloaded"
+          if docker pull ${image} | grep "Status: Downloaded"
           then
-            systemctl restart zwave-js-ui
+            systemctl restart docker-zwave-js-ui
           fi
         '';
         serviceConfig = {
