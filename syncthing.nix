ff3f3d297e7184d9556e62f52ec0c5d0ec74e60c
root <root@localhost>
2024-06-04 18:27:22 -0600

    docker service migration - syncthing

--- syncthing.nix
+++ syncthing.nix
@@ -1,5 +1,8 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+  image = "lscr.io/linuxserver/syncthing";
+in
 {
   imports = [ ./docker.nix ];
 
@@ -15,41 +18,36 @@
     '';
   };
 
+  virtualisation.oci-containers.containers.syncthing = {
+    image = "${image}";
+    autoStart = true;
+    ports = [
+      "8384:8384"
+      "22000:22000"
+      "21027:21027/udp"
+    ];
+    environment = {
+      PUID = "${toString config.users.users.josh.uid}";
+      PGID = "${toString config.users.groups.users.gid}";
+      TZ = "${config.time.timeZone}";
+      UMASK_SET = "002";
+    };
+    volumes = [
+      "/var/lib/syncthing:/config"
+      "/home/josh/sync:/shares/Sync"
+      "/d/photos:/shares/Photos"
+      "/d/software/Tools:/shares/Tools"
+      "/d/media/Comics:/shares/Comics"
+      "/d/media/Music:/shares/Music"
+    ];
+  };
+
   systemd = {
     services = {
-      syncthing = {
-        enable = true;
-        description = "Syncthing";
-        wantedBy = [ "multi-user.target" ];
-        requires = [ "network-online.target" ];
-        path = [ pkgs.docker ];
-        script = ''
-          docker container stop syncthing >/dev/null 2>&1 || true ; \
-          docker container rm -f syncthing >/dev/null 2>&1 || true ; \
-          docker run --rm --name syncthing \
-            -p 8384:8384 \
-            -p 22000:22000 \
-            -p 21027:21027/udp \
-            -e PUID=$(id -u josh) \
-            -e PGID=$(id -g josh) \
-            -e TZ=$(timedatectl show -p Timezone --value) \
-            -e UMASK_SET=002 \
-            -v /var/lib/syncthing:/config \
-            -v /home/josh/sync:/shares/Sync \
-            -v /d/photos:/shares/Photos \
-            -v /d/software/Tools:/shares/Tools \
-            -v /d/media/Comics:/shares/Comics \
-            -v /d/media/Music:/shares/Music \
-            lscr.io/linuxserver/syncthing
-        '';
-        serviceConfig = {
-          Restart = "no";
-        };
-      };
       syncthing-update = {
         path = [ pkgs.docker ];
         script = ''
-          if docker pull lscr.io/linuxserver/syncthing | grep "Status: Downloaded"
+          if docker pull ${image} | grep "Status: Downloaded"
           then
             systemctl restart syncthing
           fi
--- users.nix
+++ users.nix
@@ -7,6 +7,8 @@
   #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   # };
 
+  users.groups.users.gid = 100;
+
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGoNERTKsysxLg10U5gR/u2bAEQ1FuqLcnm0ONsGTXw4 Joshua Gustafson"
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC00AG0O5/puSgqVQeqL8e/no0NdqNrs6IET9OykhJZ+iDVK52WC+v48SZsNIHh+/3370JsSKB5FU3q0bkgFvMfk7cTg30K+yKjlamnsFn2fd/rhhO7Iadj9crD9To+LpNoiRDD0JOqPLHWXRiyOPrxBVgUvWfJptrGm0ZKtVu0rR629n5fU/l4WZZY6KAV8DvOSvRneChTLWtrAGTbSdpaDJs+ie2u9MvNULFGLbPFc1ZcPAYjlI2mewCsIRuMx4jZ3Zwn8SBa/F0idCj3SS32lZtGaDdg/iV52DrxFWhwNXI3sPAx8lem82+3Umx7blOB02whEFTrYBePPVgif3uod8k5GiNrOmAgfYbcAe7rSma+yUfujTBKtLqJogl+QGbi6gzGXkvsY3dc50nUVyDok7HFiY+HDeCdonv0iwSubG5BZD7EkqkJnDD+yvTZ/W5c+I/tYSh92x/4Mp/pnxHgguHSqZNbwAVFuR7ACfkRKSfWsr8qBP8bixpCaN+U//3UwIFJRKRbJCpO8r8v6sGF5X0yO660TcupzLOsW6qCjcVm1uOin06zcbILqyWb9CvnC/ufKH/IAKDbU3oRJELS6fwaMY/oAoSXqdlg9ABvwioVafPZkeK5thf8cpKySPd4hoxtilO3lTbxUccZsVnzkj86IxGmU95B//PLs5Ctcw== Joshua Gustafson"
