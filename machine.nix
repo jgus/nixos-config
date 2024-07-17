1c506108d257347aecca6538c2f183f473327e00
Josh Gustafson <j@gustafson.me>
2024-07-17 10:58:43 -0600

    more machines

--- machine.nix
+++ machine.nix
@@ -1,18 +1,27 @@
 # hostId: head -c4 /dev/urandom | od -A none -t x4
 let
+  machine-id = import ./.machine-id.nix; # file contains just a quoted string
   default = {
+    hostName = "${machine-id}";
     arch = "x86";
     zfs = true;
     imports = [];
   };
-  machine = default // {
-    pi-67dc75 = {
-      hostName = "pi-67dc75";
-      hostId = "39a18894";
+  zwave-box = {
       arch = "rpi";
       zfs = false;
       imports = [ ./zwave-js-ui.nix ];
+  };
+  machine = default // {
+    pi-67db40 = zwave-box // {
+      hostId = "1f758e73";
     };
-  }."${import ./.machine-id.nix}"; # file contains just a quoted string
+    pi-67dbcd = zwave-box // {
+      hostId = "da46f0cf";
+    };
+    pi-67dc75 = zwave-box // {
+      hostId = "39a18894";
+    };
+  }."${machine-id}";
 in
 machine
