1edcbadd380f1adce283008f96353eb86bd52bd5
Josh Gustafson <j@gustafson.me>
2024-07-17 07:45:34 -0600

    pi

--- configuration.nix
+++ configuration.nix
@@ -8,15 +8,13 @@
       ./interfaces.nix
 
       ./common.nix
-      ./x86.nix
+      ./rpi.nix
       ./host.nix
       ./users.nix
 
-      #./nvidia.nix
       ./vscode.nix
-      ./zfs.nix
       #./clamav.nix # needs .secrets/gmail-password.nix
 
-      #./syncthing.nix
+      ./zwave-js-ui.nix
     ];
 }
