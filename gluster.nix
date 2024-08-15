346bde3a143e7d8cb179cc5fb01f85dcfd20951f
root <root@localhost>
2024-08-15 15:29:51 -0600

    wip

--- gluster.nix
+++ gluster.nix
@@ -1,13 +1,21 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   addresses = import ./addresses.nix;
   machine = import ./machine.nix;
+  base-port = 49152;
+  max-port = 49200;
 in
 {
+  environment.etc = {
+    "glusterfs/glusterd.vol".text = ''
+      option base-port ${toString base-port}
+      option max-port ${toString max-port}
+    '';
+  };
   networking.firewall = {
-    allowedTCPPorts = [ 24007 24008 ];
-    allowedUDPPorts = [ 24007 24008 ];
+    allowedTCPPorts = [ 24007 24008 ] ++ (lib.lists.range base-port max-port);
+    allowedUDPPorts = [ 24007 24008 ] ++ (lib.lists.range base-port max-port);
   };
   services = {
     glusterfs.enable = true;
