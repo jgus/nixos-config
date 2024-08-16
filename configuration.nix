1a7d0296959b0ab8c1b666a9a03bcf942a053290
root <root@localhost>
2024-08-16 14:21:05 -0600

    hardware-configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,51 +4,50 @@ let
   machine = import ./machine.nix;
 in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      (if (machine.arch == "rpi") then ./hardware-configuration-pi.nix else ./hardware-configuration.nix)
-      ./common.nix
-      ./${machine.arch}.nix
-      ./host.nix
-      ./users.nix
-      ./vscode.nix
-      ./gluster.nix
+  imports = [
+    ./machine/${machine.hostName}/hardware-configuration.nix
+    ./common.nix
+    ./${machine.arch}.nix
+    ./host.nix
+    ./users.nix
+    ./vscode.nix
+    ./gluster.nix
 
-      ./pihole.nix
-      ./ntp.nix
-      ./landing.nix
+    ./pihole.nix
+    ./ntp.nix
+    ./landing.nix
 
-      ./nas.nix
-      ./syncthing.nix
+    ./nas.nix
+    ./syncthing.nix
 
-      ./www.nix
+    ./www.nix
 
-      ./mosquitto.nix
-      ./zigbee2mqtt.nix
-      ./zwave.nix
-      ./esphome.nix
-      ./home-assistant.nix
+    ./mosquitto.nix
+    ./zigbee2mqtt.nix
+    ./zwave.nix
+    ./esphome.nix
+    ./home-assistant.nix
 
-      ./frigate.nix
+    ./frigate.nix
 
-      ./plex.nix
+    ./plex.nix
 
-      ./sabnzbd.nix
-      ./transmission.nix
-      ./prowlarr.nix
-      ./lidarr.nix
-      ./radarr.nix
-      ./sonarr.nix
-      ./mylar.nix
-      ./komga.nix
+    ./sabnzbd.nix
+    ./transmission.nix
+    ./prowlarr.nix
+    ./lidarr.nix
+    ./radarr.nix
+    ./sonarr.nix
+    ./mylar.nix
+    ./komga.nix
 
-      ./minecraft.nix
+    ./minecraft.nix
 
-      ./userbox.nix
-    ]
-    ++ (if machine.nvidia then [ ./nvidia.nix ] else [])
-    ++ (if machine.zfs then [ ./zfs.nix ] else [])
-    ++ (if machine.clamav then [ ./clamav.nix ] else [])
-    ++ (if machine.python then [ ./python.nix ] else [])
-    ++ machine.imports;
+    ./userbox.nix
+  ]
+  ++ (if machine.nvidia then [ ./nvidia.nix ] else [])
+  ++ (if machine.zfs then [ ./zfs.nix ] else [])
+  ++ (if machine.clamav then [ ./clamav.nix ] else [])
+  ++ (if machine.python then [ ./python.nix ] else [])
+  ++ machine.imports;
 }
