47bd783774fd2d3007f261d09b5bfdc1cd0a39ae
root <root@localhost>
2024-08-19 14:53:36 -0600

    plex

--- plex.nix
+++ plex.nix
@@ -3,6 +3,7 @@
 with (import ./functions.nix) { inherit pkgs; };
 let
   service = "plex";
+  serviceMount = "var-lib-${builtins.replaceStrings ["-"] ["\\x2d"] service}.mount";
   user = "plex";
   group = "plex";
   image = "lscr.io/linuxserver/plex";
@@ -34,22 +35,16 @@ if (machine.hostName != addresses.records."${service}".host) then {} else
     ];
   };
 
+  fileSystems."/var/lib/${service}" = {
+    device = "localhost:/varlib-${service}";
+    fsType = "glusterfs";
+  };
+
   systemd = {
     services = docker-services {
       name = service;
       image = image;
-      setup-script = ''
-        if ! zfs list r/varlib/${service} >/dev/null 2>&1
-        then
-          zfs create r/varlib/${service}
-          chown ${user}:${group} /var/lib/${service}
-          rsync -arPx --delete /nas/backup/varlib/${service}/ /var/lib/${service}/ || true
-        fi
-      '';
-      backup-script = ''
-        mkdir -p /nas/backup/varlib/${service}
-        rsync -arPx --delete /var/lib/${service}/ /nas/backup/varlib/${service}/
-      '';
+      requires = [ serviceMount "nas.mount" ];
     };
   };
 }
