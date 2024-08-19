0356a3d3beb25d913a25b3fc7047eb801a3ec3e7
root <root@localhost>
2024-08-19 15:25:35 -0600

    www

--- www.nix
+++ www.nix
@@ -12,6 +12,16 @@ if (machine.hostName != addresses.records.web-swag.host) then {} else
 {
   imports = [ ./docker.nix ];
 
+  fileSystems = builtins.listToAttrs (map (name:
+    {
+      name = name;
+      value = {
+        device = "localhost:/varlib-${name}";
+        fsType = "glusterfs";
+      };
+    }
+  ) [ "www" "dav" "swag_config" "web_db_data" "web_db_admin_sessions" ]);
+
   virtualisation.oci-containers.containers.web-db = {
     image = db_image;
     autoStart = true;
@@ -73,73 +83,15 @@ if (machine.hostName != addresses.records.web-swag.host) then {} else
     services = docker-services {
       name = "web-db";
       image = db_image;
-      setup-script = ''
-        if ! zfs list r/varlib/web_db_data >/dev/null 2>&1
-        then
-          zfs create r/varlib/web_db_data
-          rsync -arPx --delete /nas/backup/varlib/web_db_data/ /var/lib/web_db_data/ || true
-        fi
-      '';
-      backup-script = ''
-        mkdir -p /nas/backup/varlib/web_db_data
-        rsync -arPx --delete /var/lib/web_db_data/ /nas/backup/varlib/web_db_data/
-      '';
+      requires = [ "var-lib-web_db_data.mount" ];
     } // docker-services {
       name = "web-db-admin";
       image = db_admin_image;
-      setup-script = ''
-        if ! zfs list r/varlib/web_db_admin_sessions >/dev/null 2>&1
-        then
-          zfs create r/varlib/web_db_admin_sessions
-          rsync -arPx --delete /nas/backup/varlib/web_db_admin_sessions/ /var/lib/web_db_admin_sessions/ || true
-        fi
-      '';
-      backup-script = ''
-        mkdir -p /nas/backup/varlib/web_db_admin_sessions
-        rsync -arPx --delete /var/lib/web_db_admin_sessions/ /nas/backup/varlib/web_db_admin_sessions/
-      '';
+      requires = [ "var-lib-web_db_admin_sessions.mount" ];
     } // docker-services {
       name = "web-swag";
       image = swag_image;
-      setup-script = ''
-        if ! zfs list r/varlib/web_proxy_config >/dev/null 2>&1
-        then
-          zfs create r/varlib/web_proxy_config
-          rsync -arPx --delete /nas/backup/varlib/web_proxy_config/ /var/lib/web_proxy_config/ || true
-        fi
-        if ! zfs list r/varlib/swag_config >/dev/null 2>&1
-        then
-          zfs create r/varlib/swag_config
-          rsync -arPx --delete /nas/backup/varlib/swag_config/ /var/lib/swag_config/ || true
-          mkdir -p /var/lib/swag_config/keys
-          mkdir -p /var/lib/swag_config/etc/letsencrypt
-        fi
-        if ! zfs list r/varlib/www >/dev/null 2>&1
-        then
-          zfs create r/varlib/www
-          rsync -arPx --delete /nas/backup/varlib/www/ /var/lib/www/ || true
-        fi
-        if ! zfs list r/varlib/dav >/dev/null 2>&1
-        then
-          zfs create r/varlib/dav
-          mkdir -p /var/lib/dav/tmp
-          mkdir -p /var/lib/dav/files
-          chown www:www /var/lib/dav
-          chown www:www /var/lib/dav/tmp
-          chown www:www /var/lib/dav/files
-          rsync -arPx --delete /nas/backup/varlib/dav/ /var/lib/dav/ || true
-        fi
-      '';
-      backup-script = ''
-        mkdir -p /nas/backup/varlib/web_proxy_config
-        rsync -arPx --delete /var/lib/web_proxy_config/ /nas/backup/varlib/web_proxy_config/
-        mkdir -p /nas/backup/varlib/swag_config
-        rsync -arPx --delete /var/lib/swag_config/ /nas/backup/varlib/swag_config/
-        mkdir -p /nas/backup/varlib/www
-        rsync -arPx --delete /var/lib/www/ /nas/backup/varlib/www/
-        mkdir -p /nas/backup/varlib/dav
-        rsync -arPx --delete /var/lib/dav/ /nas/backup/varlib/dav/
-      '';
+      requires = [ "var-lib-swag_config.mount" "var-lib-www.mount" "var-lib-dav.mount" "nas.mount" ];
     };
   };
 }
