4553abce415a02e6e85d1ac705c30d3c406541bd
root <root@localhost>
2024-08-27 13:46:57 -0600

    cleanup

--- landing.nix
+++ landing.nix
@@ -3,6 +3,40 @@
 with (import ./functions.nix) { inherit pkgs; };
 let
   pubkeys = import ./pubkeys.nix;
+  dockerfile = pkgs.writeText "Dockerfile" ''
+    FROM alpine
+
+    RUN apk add --no-cache openssh tini
+    RUN rm -rf /etc/ssh
+    COPY entrypoint.sh /
+
+    EXPOSE 22/tcp
+
+    ENTRYPOINT ["tini", "--", "/entrypoint.sh"]
+    CMD []
+  '';
+  entrypoint = pkgs.writeText "entrypoint.sh" ''
+    #!/bin/sh
+
+    adduser -D user
+    passwd -ud user
+    mkdir /home/user/.ssh
+    echo "${pubkeys.backup-rsa}" >/home/user/.ssh/authorized_keys
+    chmod 700 /home/user/.ssh
+    chmod 400 /home/user/.ssh/authorized_keys
+    chown -R user:user /home/user/.ssh
+
+    ssh-keygen -A
+
+    /usr/sbin/sshd -D -e
+  '';
+  sshd_config = pkgs.writeText "sshd_config" ''
+    PermitRootLogin no
+    AuthorizedKeysFile .ssh/authorized_keys
+    PasswordAuthentication no
+    AllowTcpForwarding yes
+    Subsystem sftp /usr/lib/ssh/sftp-server
+  '';
 in
 {
   imports = [(homelabService {
@@ -13,16 +47,21 @@ in
       script = { dockerOptions, ... }: ''
         docker container stop landing >/dev/null 2>&1 || true ; \
         docker container rm -f landing >/dev/null 2>&1 || true ; \
-        rsync -arPL /etc/landing /tmp/
-        rsync -arPL /etc/nixos/.secrets/landing /tmp/
-        chmod -R 400 /tmp/landing/etc/ssh
-        chmod a+x /tmp/landing/docker/entrypoint.sh
-        docker build -t ssh /tmp/landing/docker
+        BUILD_DIR=$(mktemp -d)
+        cp ${dockerfile} ''${BUILD_DIR}/Dockerfile
+        cp ${entrypoint} ''${BUILD_DIR}/entrypoint.sh
+        chmod a+x ''${BUILD_DIR}/entrypoint.sh
+        docker build -t landing ''${BUILD_DIR}
+        rm -rf ''${BUILD_DIR}
+        SSH_DIR=$(mktemp -d)
+        rsync -arPL ${./.secrets/landing/etc/ssh}/ ''${SSH_DIR}/
+        cp ${sshd_config} ''${SSH_DIR}/sshd_config
+        chmod -R 400 ''${SSH_DIR}
         /bin/sh -c "docker run --rm --name landing \
           ${builtins.concatStringsSep " " dockerOptions} \
-          -e AUTHORIZED_KEYS='${pubkeys.backup-rsa}' \
-          -v /tmp/landing/etc/ssh:/etc/ssh \
-          ssh"
+          -v ''${SSH_DIR}:/etc/ssh \
+          landing"
+        rm -rf ''${SSH_DIR}
       '';
       unitConfig = {
         StartLimitIntervalSec = 0;
@@ -34,42 +73,6 @@ in
     };
     extraConfig = {
       imports = [ ./docker.nix ];
-      environment.etc = {
-        "landing/docker/Dockerfile".text = ''
-          FROM alpine
-
-          RUN apk add --no-cache openssh tini
-          RUN rm -rf /etc/ssh
-          COPY entrypoint.sh /
-
-          EXPOSE 22/tcp
-
-          ENTRYPOINT ["tini", "--", "/entrypoint.sh"]
-          CMD []
-        '';
-        "landing/docker/entrypoint.sh".text = ''
-          #!/bin/sh
-
-          adduser -D user
-          passwd -ud user
-          mkdir /home/user/.ssh
-          echo "''${AUTHORIZED_KEYS}" >/home/user/.ssh/authorized_keys
-          chmod 700 /home/user/.ssh
-          chmod 400 /home/user/.ssh/authorized_keys
-          chown -R user:user /home/user/.ssh
-
-          ssh-keygen -A
-
-          /usr/sbin/sshd -D -e
-        '';
-        "landing/etc/ssh/sshd_config".text = ''
-          PermitRootLogin no
-          AuthorizedKeysFile .ssh/authorized_keys
-          PasswordAuthentication no
-          AllowTcpForwarding yes
-          Subsystem sftp /usr/lib/ssh/sftp-server
-        '';
-      };
     };
   })];
 }
