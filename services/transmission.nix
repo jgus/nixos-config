40d91edfd2a6dde89fe31c04396063505188c89c
root <root@localhost>
2024-09-02 23:59:19 -0600

    transmission

--- services/transmission.nix
+++ services/transmission.nix
@@ -2,28 +2,15 @@ let
   user = "josh";
   group = "plex";
 in
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   requires = [ "storage-scratch.mount" ];
   docker = {
-    image = "haugene/transmission-openvpn";
-    environmentFiles = [
-      ./../.secrets/privadovpn.env
-    ];
+    image = "lscr.io/linuxserver/transmission";
     environment = {
       PUID = toString config.users.users.${user}.uid;
       PGID = toString config.users.groups.${group}.gid;
       TZ = config.time.timeZone;
-      OPENVPN_PROVIDER = "PRIVADO";
-      LOCAL_NETWORK = "172.22.0.0/16";
-      TRANSMISSION_DOWNLOAD_DIR = "/peer/Complete";
-      TRANSMISSION_INCOMPLETE_DIR = "/peer/Incomplete";
-      TRANSMISSION_INCOMPLETE_DIR_ENABLED = "true";
-      TRANSMISSION_WATCH_DIR = "/peer/Watch";
-      TRANSMISSION_WATCH_DIR_ENABLED = "true";
-      TRANSMISSION_DOWNLOAD_QUEUE_ENABLED = "false";
-      TRANSMISSION_BLOCKLIST_ENABLED = "true";
-      TRANSMISSION_BLOCKLIST_URL = "https://github.com/Naunter/BT_BlockLists/raw/master/bt_blocklists.gz";
     };
     ports = [
       "9091"
@@ -31,7 +18,6 @@ in
     ];
     configVolume = "/config";
     volumes = [
-      "/etc/localtime:/etc/localtime:ro"
       "/storage/scratch/peer:/peer"
     ];
     extraOptions = [
