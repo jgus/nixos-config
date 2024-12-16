b2ac692ff9af87a698571600443cee2e1600756b
root <root@localhost>
2024-12-15 19:17:30 -0700

    nividia

--- nvidia.nix
+++ nvidia.nix
@@ -2,8 +2,9 @@
 {
   nixpkgs.config.allowUnfree = true;
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport32Bit = true;
+  hardware.graphics.enable = true;
+  # hardware.graphics.enable32Bit = true;
+  hardware.nvidia.open = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   hardware.nvidia-container-toolkit.enable = true;
 }
