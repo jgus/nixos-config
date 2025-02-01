bd7e52789e5a6a9f215ee28a7a1ed0bb0ca311bc
root <root@localhost>
2025-02-01 09:40:03 -0700

    ntp

--- services/ntp.nix
+++ services/ntp.nix
@@ -6,6 +6,10 @@
     ports = [
       "123/udp"
     ];
+    environment = {
+      NTP_SERVERS = "time.cloudflare.com";
+      ENABLE_NTS = "true";
+    };
     extraOptions = [
       "--read-only"
       "--tmpfs=/etc/chrony:rw,mode=1750"
