d197e0566836ad6fa1f16a71a2581524a344e65b
root <root@localhost>
2025-07-28 13:27:58 -0600

    bump jeepney

--- systemctl-mqtt.nix
+++ systemctl-mqtt.nix
@@ -2,25 +2,6 @@
 let
   pw = import ./.secrets/passwords.nix;
 
-  python = pkgs.python3.override {
-    self = python;
-    packageOverrides = pyfinal: pyprev: {
-      jeepney = pyprev.jeepney.overridePythonAttrs (old: rec {
-        version = "0.8.0";
-        src = pkgs.fetchPypi {
-          pname = "jeepney";
-          inherit version;
-          hash = "sha256-Xv5I0lWXOQL2utw85V4qpsXDs7xkIFnvOpEke8/MWAY=";
-        };
-        propagatedBuildInputs = with pyprev; [
-          outcome
-          trio
-        ];
-        doCheck = false;
-      });
-    };
-  };
-
   systemctlMqttPackage =
     { buildPythonApplication
     , fetchFromGitHub
@@ -36,8 +17,8 @@ let
       src = fetchFromGitHub {
         owner = "jgus";
         repo = "systemctl-mqtt";
-        rev = "e69f34c444dbb3a5fffd113e9439e46068c256ab";
-        sha256 = "sha256-cDTjgJDCTFn/fQ4Q09txdZbM0iiFm+OSHgDI1L7OP2Q=";
+        rev = "4e8e9acc4ae8d46b543b2d9f4ab51d75728e1a2f";
+        sha256 = "sha256-UH8GdLBkee0RwGi1Ee5nZf/fF4LdeDPUsayvOOj46Ks=";
       };
       dependencies = [
         aiomqtt
@@ -51,14 +32,13 @@ let
         wheel
       ];
     };
-
 in
 {
   systemd.services = {
     systemctl-mqtt = {
       path = [
         pkgs.gawk
-        (python.pkgs.callPackage systemctlMqttPackage { })
+        (pkgs.python3.pkgs.callPackage systemctlMqttPackage { })
       ];
       script = ''
         systemctl-mqtt --mqtt-host mqtt.home.gustafson.me --mqtt-disable-tls --mqtt-username server --mqtt-password ${pw.mqtt.server} $(systemctl list-units --full --plain --no-legend --output=short docker-*.service | awk '{print "--monitor-system-unit " $1 " --control-system-unit " $1}')
