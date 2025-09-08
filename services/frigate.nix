bd5832724bf36ae0b1881744511acf17a5f5e02a
root <root@localhost>
2025-09-08 09:50:48 -0600

    frigate model

--- services/frigate.nix
+++ services/frigate.nix
@@ -171,7 +171,7 @@ let
       onnx1 = { type = "onnx"; device = "1"; };
     } else { });
     model = (if (detector == "coral") then {
-      path = "plus://89f920ad4116b84bfa004741fef7db93"; # custom 8/19/2025
+      path = "plus://5a970c3ac370ce721539812ab83f22eb"; # custom 9/7/2025
     } else if (detector == "onnx") then {
       # path = "plus://717c77b1c548a9f5371b44e1ec8466fd"; # stock 2025.2
       path = "plus://e6a3aa40b94b1494f5beac6f427343cb"; # custom 8/13/2025
