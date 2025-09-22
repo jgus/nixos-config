675f0f1892c80538f090194754af26fabd2fd253
root <root@localhost>
2025-09-22 10:14:16 -0700

    vscode

--- vscode.nix
+++ vscode.nix
@@ -1,9 +1,4 @@
-{ pkgs, ... }:
-
+{ ... }:
 {
-  imports = [ (fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master") ];
-
-  services = {
-    vscode-server.enable = true;
-  };
+  programs.nix-ld.enable = true;
 }
