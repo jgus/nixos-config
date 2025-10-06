b04ea6e9214ea680d80554250fc0f3e77c32f701
root <root@localhost>
2025-10-06 10:09:15 -0600

    ollama

--- services/ollama.nix
+++ services/ollama.nix
@@ -6,6 +6,9 @@
       "11434"
     ];
     configVolume = "/root/.ollama";
+    volumes = [
+      "/d/ollama/models:/root/.ollama/models"
+    ];
     extraOptions = [
       "--device=nvidia.com/gpu=all"
     ];
--- services/open-webui.nix
+++ services/open-webui.nix
@@ -1,13 +1,17 @@
 { ... }:
 {
   docker = {
-    image = "ghcr.io/open-webui/open-webui:main";
+    image = "ghcr.io/open-webui/open-webui:cuda";
     ports = [
       "8080"
     ];
     environment = {
-      OLLAMA_BASE_URL = "http://ollama:11434";
+      # OLLAMA_BASE_URL = "http://ollama:11434";
+      OLLAMA_BASE_URL = "http://josh-pc:11434";
     };
     configVolume = "/app/backend/data";
+    extraOptions = [
+      "--device=nvidia.com/gpu=all"
+    ];
   };
 }
