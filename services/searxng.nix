d1c687e8230d2492e571a7a8dd6089ef29490682
root <root@localhost>
2025-10-09 08:38:55 -0600

    searxng

--- services/searxng.nix
+++ services/searxng.nix
@@ -1,24 +1,28 @@
 let
   pw = import ./../.secrets/passwords.nix;
+  settings = {
+    use_default_settings = true;
+    server = {
+      base_url = "https://search.gustafson.me";
+      port = "8080";
+      bind_address = "0.0.0.0";
+      secret_key = pw.searxng.secret;
+    };
+    search = {
+      formats = [ "html" "json" ];
+    };
+  };
 in
-{ config, ... }:
+{ pkgs, ... }:
 {
   docker = {
     image = "docker.io/searxng/searxng";
-    environment = {
-      # PUID = toString config.users.users.${user}.uid;
-      # PGID = toString config.users.groups.${group}.gid;
-      # TZ = config.time.timeZone;
-      # USER = "josh";
-      # PASS = pw.transmission;
-      SEARXNG_BASE_URL = "http://searxng.home.gustafson.me:8080";
-      SEARXNG_PORT = "8080";
-      SEARXNG_BIND_ADDRESS = "0.0.0.0";
-      SEARXNG_SECRET = pw.searxng.secret;
-    };
     ports = [
       "8080"
     ];
     configVolume = "/var/cache/searxng";
+    volumes = [
+      "${(pkgs.formats.yaml { }).generate "settings.yml" settings}:/etc/searxng/settings.yml:ro"
+    ];
   };
 }
