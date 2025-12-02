ca17c3d28adc1e242eb778c20c8065e6ac03c1e5
root <root@localhost>
2025-12-02 15:56:27 -0700

    nvidia docker wip

--- nvidia.nix
+++ nvidia.nix
@@ -4,19 +4,20 @@
   nixpkgs.config.cudaSupport = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.graphics.enable = true;
-  # hardware.graphics.enable32Bit = true;
+  hardware.graphics.enable32Bit = true;
   hardware.nvidia.open = false;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   hardware.nvidia-container-toolkit = {
     enable = true;
-    mount-nvidia-docker-1-directories = true;
-    mount-nvidia-executables = true;
-    mounts = [
-      {
-        hostPath = "/run/opengl-driver/lib/libcuda.so";
-        containerPath = "/usr/lib/libcuda.so";
-      }
-    ];
+    # mount-nvidia-docker-1-directories = true;
+    # mount-nvidia-executables = true;
+    # mounts = [
+    #   {
+    #     hostPath = "/run/opengl-driver/lib/libcuda.so";
+    #     containerPath = "/usr/lib/libcuda.so";
+    #   }
+    # ];
   };
-  virtualisation.docker.daemon.settings.features.cdi = true;
+  # virtualisation.docker.daemon.settings.features.cdi = true;
+  virtualisation.docker.enableNvidia = true;
 }
