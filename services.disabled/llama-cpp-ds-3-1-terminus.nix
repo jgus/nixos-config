f7ff9c87f78e642c6886979ff4524d7f08e6e9b4
root <root@localhost>
2025-12-09 20:10:38 -0700

    disable

---
+++ services.disabled/llama-cpp-ds-3-1-terminus.nix
@@ -0,0 +1,38 @@
+{ ... }:
+{
+  configStorage = false;
+  docker = {
+    image = "ghcr.io/ggml-org/llama.cpp:server-cuda";
+    ports = [
+      "8080"
+    ];
+    volumes = [
+      "/d/llama.cpp:/root/.cache/llama.cpp"
+    ];
+    extraOptions = [
+      "--read-only"
+      "--device=nvidia.com/gpu=GPU-8bb9f199-be89-462d-8e68-6ba4fe870ce4"
+    ];
+    entrypointOptions = [
+      "--threads"
+      "72"
+      # "-np"
+      # "4"
+      "-hf"
+      "unsloth/DeepSeek-V3.1-Terminus-GGUF:UD-Q2_K_XL"
+      "--jinja"
+      "--temp"
+      "0.6"
+      "--top-p"
+      "0.95"
+      "--min-p"
+      "0.01"
+      "--batch-size"
+      "512"
+      "--ctx-size"
+      (toString (32 * 1024))
+      "-ot"
+      ".ffn_.*_exps.=CPU"
+    ];
+  };
+}
