755fe714f172518459cdc2e1e24e058478d31e6a
root <root@localhost>
2025-12-11 15:17:44 -0700

    d1 zfs

--- machine/d1/format-d.sh
+++ machine/d1/format-d.sh
@@ -15,21 +15,15 @@ ZPOOL_OPTS=(
     -O autobackup:snap-$(hostname)=true
 )
 
-# D_DISKS=(
-#     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:18:0
-#     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:19:0
-#     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:20:0
-#     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:21:0
-#     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:22:0
-#     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:23:0
-# )
+# Available
+    # /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:0:0
+    # /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:1:0
+    # /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:2:0
+    # /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:3:0
+    # /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:4:0
 
-F_DISKS=(
-    /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:0:0
-    /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:1:0
-    /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:2:0
-    /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:3:0
-    /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:4:0
+
+D_DISKS=(
     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:5:0
     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:6:0
     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:7:0
@@ -43,9 +37,10 @@ F_DISKS=(
     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:15:0
     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:16:0
     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:17:0
+    /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:18:0
 )
 
-F2_DISKS=(
+F_DISKS=(
     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:19:0
     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:20:0
     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:21:0
@@ -53,12 +48,7 @@ F2_DISKS=(
     /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:23:0
 )
 
-# Available:
-#    /dev/disk/by-path/pci-0000:03:00.0-scsi-0:0:18:0
 
+zpool create -f "${ZPOOL_OPTS[@]}" -O recordsize=128K -O mountpoint=/d d raidz3 "${D_DISKS[@]}"
 
-# zpool create -f "${ZPOOL_OPTS[@]}" -O recordsize=16K -O mountpoint=/d d raidz2 "${D_DISKS[@]}"
-
-zpool create -f "${ZPOOL_OPTS[@]}" -O recordsize=64K -O mountpoint=/f f raidz2 "${F_DISKS[@]}"
-
-zpool create -f "${ZPOOL_OPTS[@]}" -O recordsize=16K -O mountpoint=/f2 f2 raidz "${F2_DISKS[@]}"
+zpool create -f "${ZPOOL_OPTS[@]}" -O recordsize=16K -O mountpoint=/f f raidz "${F_DISKS[@]}"
