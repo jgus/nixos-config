90bf307f646879ecccf71a7f8015619586493b51
root <root@localhost>
2025-12-26 11:04:45 -0700

    landing

--- services/landing.nix
+++ services/landing.nix
@@ -3,38 +3,51 @@ let
   user = "user";
   group = "users";
 
-  # SSH configuration
-  sshd_config = pkgs.writeText "sshd_config" ''
-    PermitRootLogin no
-    AuthorizedKeysFile .ssh/authorized_keys
-    PasswordAuthentication no
-    AllowTcpForwarding yes
-    Subsystem sftp internal-sftp
-    HostKey /etc/ssh/ssh_host_rsa_key
-    HostKey /etc/ssh/ssh_host_ecdsa_key
-    HostKey /etc/ssh/ssh_host_ed25519_key
-  '';
+  files = {
+    etc_passwd = pkgs.writeText "etc_passwd" ''
+      ${user}:x:1000:1000:${group}:/home/${user}:/bin/bash
+      sshd:x:74:74:Privilege-separated SSH:/var/empty/sshd:/sbin/nologin
+    '';
+
+    etc_group = pkgs.writeText "etc_group" ''
+      ${group}:x:1000:
+      sshd:x:74:
+    '';
+
+    etc_shadow = pkgs.writeText "etc_shadow" ''
+      ${user}:*:1::::::
+    '';
+
+    # SSH configuration
+    sshd_config = pkgs.writeText "sshd_config" ''
+      PermitRootLogin no
+      AuthorizedKeysFile .ssh/authorized_keys
+      PasswordAuthentication no
+      AllowTcpForwarding yes
+      Subsystem sftp internal-sftp
+      HostKey /etc/ssh/ssh_host_rsa_key
+      HostKey /etc/ssh/ssh_host_ecdsa_key
+      HostKey /etc/ssh/ssh_host_ed25519_key
+    '';
+  };
 
   # All file setup with permissions (chmod works here, only chown needs fakeroot)
   etcFiles = pkgs.runCommand "landing-etc" { } ''
-    mkdir -p $out/etc/ssh
-    mkdir -p $out/home/${user}/.ssh
     mkdir -p $out/var/empty
 
     # User files
-    echo "${user}:x:1000:1000:${group}:/home/${user}:/bin/bash" > $out/etc/passwd
-    echo "sshd:x:74:74:Privilege-separated SSH:/var/empty/sshd:/sbin/nologin" >> $out/etc/passwd
-    echo "${group}:x:1000:" > $out/etc/group
-    echo "sshd:x:74:" >> $out/etc/group
-    echo "${user}:*:1::::::" > $out/etc/shadow
+    mkdir -p $out/etc
+    cp ${files.etc_passwd} $out/etc/passwd
+    cp ${files.etc_group} $out/etc/group
+    cp ${files.etc_shadow} $out/etc/shadow
 
     # SSH config
-    cp ${sshd_config} $out/etc/ssh/sshd_config
-
-    # SSH host keys
+    mkdir -p $out/etc/ssh
+    cp ${files.sshd_config} $out/etc/ssh/sshd_config
     cp ${./../.secrets/landing/etc/ssh}/ssh_host_* $out/etc/ssh/
 
     # User's authorized_keys
+    mkdir -p $out/home/${user}/.ssh
     cp ${./../pubkeys/backup-rsa} $out/home/${user}/.ssh/authorized_keys
   '';
 
@@ -42,10 +55,10 @@ let
     name = "landing";
     tag = "latest";
 
-    contents = [
-      pkgs.openssh
-      pkgs.tini
-      pkgs.bashInteractive
+    contents = with pkgs; [
+      bashInteractive
+      openssh
+      tini
     ];
 
     # fakeRootCommands: copy our etc files and set ownership
