9a49d46c46482d1c8a58c5d82b21ed07694fd23c
root <root@localhost>
2025-12-25 22:48:06 -0700

    landing

--- services/landing.nix
+++ services/landing.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 let
+  user = "user";
+  group = "users";
+
   # SSH configuration
   sshd_config = pkgs.writeText "sshd_config" ''
     PermitRootLogin no
@@ -12,35 +15,27 @@ let
     HostKey /etc/ssh/ssh_host_ed25519_key
   '';
 
-  authorizedKeys = builtins.readFile ./../pubkeys/backup-rsa;
-  sshHostKeys = ./../.secrets/landing/etc/ssh;
-
   # All file setup with permissions (chmod works here, only chown needs fakeroot)
   etcFiles = pkgs.runCommand "landing-etc" { } ''
     mkdir -p $out/etc/ssh
-    mkdir -p $out/home/user/.ssh
+    mkdir -p $out/home/${user}/.ssh
     mkdir -p $out/var/empty
 
-    # User files - minimal, just what we need
-    echo "user:x:1000:1000:user:/home/user:/bin/bash" > $out/etc/passwd
+    # User files
+    echo "${user}:x:1000:1000:${group}:/home/${user}:/bin/bash" > $out/etc/passwd
     echo "sshd:x:74:74:Privilege-separated SSH:/var/empty/sshd:/sbin/nologin" >> $out/etc/passwd
-    echo "user:x:1000:" > $out/etc/group
+    echo "${group}:x:1000:" > $out/etc/group
     echo "sshd:x:74:" >> $out/etc/group
-    echo "user:*:1::::::" > $out/etc/shadow
+    echo "${user}:*:1::::::" > $out/etc/shadow
 
     # SSH config
     cp ${sshd_config} $out/etc/ssh/sshd_config
 
-    # SSH host keys with correct permissions
-    cp ${sshHostKeys}/ssh_host_rsa_key $out/etc/ssh/
-    cp ${sshHostKeys}/ssh_host_rsa_key.pub $out/etc/ssh/
-    cp ${sshHostKeys}/ssh_host_ecdsa_key $out/etc/ssh/
-    cp ${sshHostKeys}/ssh_host_ecdsa_key.pub $out/etc/ssh/
-    cp ${sshHostKeys}/ssh_host_ed25519_key $out/etc/ssh/
-    cp ${sshHostKeys}/ssh_host_ed25519_key.pub $out/etc/ssh/
+    # SSH host keys
+    cp ${./../.secrets/landing/etc/ssh}/ssh_host_* $out/etc/ssh/
 
     # User's authorized_keys
-    echo "${authorizedKeys}" > $out/home/user/.ssh/authorized_keys
+    cp ${./../pubkeys/backup-rsa} $out/home/${user}/.ssh/authorized_keys
   '';
 
   landingImage = pkgs.dockerTools.buildLayeredImage {
@@ -58,16 +53,11 @@ let
       rm -rf ./etc/ssh
       cp -rL ${etcFiles}/* ./
       chmod 640 ./etc/shadow
-      chmod 644 ./etc/ssh/sshd_config
-      chmod 600 ./etc/ssh/ssh_host_rsa_key
-      chmod 600 ./etc/ssh/ssh_host_ecdsa_key
-      chmod 600 ./etc/ssh/ssh_host_ed25519_key
-      chmod 644 ./etc/ssh/ssh_host_rsa_key.pub
-      chmod 644 ./etc/ssh/ssh_host_ecdsa_key.pub
-      chmod 644 ./etc/ssh/ssh_host_ed25519_key.pub
-      chmod 700 ./home/user/.ssh
-      chmod 600 ./home/user/.ssh/authorized_keys
-      chown -R 1000:1000 ./home/user
+      chmod 644 ./etc/ssh/*
+      chmod 600 ./etc/ssh/ssh_host_*_key
+      chmod 700 ./home/${user}/.ssh
+      chmod 600 ./home/${user}/.ssh/authorized_keys
+      chown -R 1000:1000 ./home/${user}
     '';
 
     config = {
