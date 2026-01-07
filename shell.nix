9915103ee2c64f1db24fbfb3ff37a562bb812aad
root <root@localhost>
2026-01-07 15:42:54 -0700

    Revert "Aider wip"
    This reverts commit 11c80d1072b2ff5373ca29db6e77e05b60da64fb.

--- shell.nix
+++ shell.nix
@@ -1,15 +1,9 @@
 { pkgs ? import <nixpkgs> { } }:
-let
-  pw = import ./.secrets/passwords.nix;
-in
+
 pkgs.mkShellNoCC {
   buildInputs = with pkgs; [
-    aider-chat-full
     (writeScriptBin "nixos-deploy-all" (builtins.readFile ./bin/nixos-deploy-all.sh))
   ];
-
-  OPENAI_API_BASE = "https://nano-gpt.com/api/v1";
-  OPENAI_API_KEY = pw.nanoGpt.apiKey;
 
   shellHook = ''
     echo "🎉 Welcome to the nixos-config project."
