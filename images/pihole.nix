7197c921b513018d6607bae3d5d67ffbbc125365
root <root@localhost>
2026-01-07 13:55:23 -0700

    Oops

---
+++ images/pihole.nix
@@ -0,0 +1,7 @@
+{
+  imageName = "pihole/pihole";
+  imageDigest = "sha256:91dc91ddd413bf461c283204558f8f21839851e9824799075a7ceff7c77eea40";
+  hash = "sha256-/+YRujV4n/ISyAD4LHBS1EnHwx4i6rahakbRaBIfSN0=";
+  finalImageName = "pihole/pihole";
+  finalImageTag = "latest";
+}
