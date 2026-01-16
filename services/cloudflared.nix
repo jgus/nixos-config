a968d55f8984fd35e1a9b36d795f1af388002754
root <root@localhost>
2026-01-15 19:37:45 -0700

    claoudflared sops

--- services/cloudflared.nix
+++ services/cloudflared.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 {
   configStorage = false;
   container = {
@@ -7,8 +7,12 @@
       "tunnel"
       "--no-autoupdate"
       "run"
-      "--token"
-      "eyJhIjoiNDg2M2FkMjU2YjEzNjdhNTU5OGI2YjMwMzA2MTMzZDgiLCJ0IjoiYmQ2NzFhMzYtMGNjOC00OTJiLWE4YjYtZTcwYWUwODBkMDdlIiwicyI6IlptTTRNalkxWVdFdE16WTNPUzAwTmpnekxUaGtZemt0WWpNMk1tUm1PVGRrWW1aayJ9"
     ];
+    environmentFiles = [
+      config.sops.secrets."cloudflared/env".path
+    ];
+  };
+  extraConfig = {
+    sops.secrets."cloudflared/env" = { };
   };
 }
