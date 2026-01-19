75f298a0a8192627410493676fc3f18f493a032e
root <root@localhost>
2026-01-19 10:15:41 -0700

    Add podman networks

--- settings/container.nix
+++ settings/container.nix
@@ -1,3 +1,4 @@
+with builtins;
 { addresses, lib, machine, pkgs, ... }:
 rec {
   executable = "podman";
@@ -29,11 +30,18 @@ rec {
             before = [ "${executable}.service" ];
           };
         } // {
-        podman-configure = {
+        podman-configure = lib.mkOverride 1500 {
           path = [ package ];
-          script = ''
-            ${executable} network ls --format "{{.Name}}" | grep "^hostlan$" || ${executable} network create -d macvlan --ipv6 --subnet=${addresses.network.net4} --gateway=${addresses.network.defaultGateway} --subnet=${addresses.network.net6} -o parent=br0 hostlan
-          '';
+          script =
+            let
+              suffixes = lib.concatStringsSep " " (map (vlan: ".${toString vlan.vlanId}") (attrValues addresses.vlans));
+            in
+            ''
+              for SUFFIX in "" ${suffixes}
+              do
+                ${executable} network ls --format "{{.Name}}" | grep "^hostlan''${SUFFIX}$" || ${executable} network create -d macvlan --ipv6 --subnet=${addresses.network.net4} --gateway=${addresses.network.defaultGateway} --subnet=${addresses.network.net6} -o parent=br0''${SUFFIX} hostlan''${SUFFIX}
+              done
+            '';
           serviceConfig = {
             Type = "oneshot";
           };
